cf0317785d0be681bfba49095344f77759011ad3
Derrick Ward <Derrick@Derricks-MacBook-Pro.local>
2016-12-04 13:36:39 -0500

    Created a function to Demonstrate how to create a view and how to create a view in a view.

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Perform the Actions of the super class first, if there were any
+        super.viewDidLoad();
+        
+        demoViews();
     }
+    
+    
+    //Demos Creating a View and a subview of that view
+    func demoViews(){
+        //Create a CGRect/Frame, containing position and size information
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150);
+        
+        //Create a UIView, defined by our custom Frame above
+        let firstView = UIView(frame: firstFrame);
+        
+        //Set the Background Color of our view to blue
+        firstView.backgroundColor = UIColor.blue;
+        
+        //Add our view to this Controller's view
+        view.addSubview(firstView);
+        
+        
+        
+        /*
+         * Let's create a Second Frame and View and make a subview of the first we created
+         */
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 75);
+        let secondView = UIView(frame: secondFrame);
+        secondView.backgroundColor = UIColor.yellow;
+        firstView.addSubview(secondView);
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
